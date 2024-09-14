99b4b9ee0ebf6207617de017b8d7ca6b3685f612
Weber Yang <weberyanglalala@gmail.com>
2024-09-14 18:42:48 +0800

    project init

---
+++ Dotnet8DifyAgentSample/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Dotnet8DifyAgentSample.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
