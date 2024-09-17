6c8ebcc282ef0ba82500c37ffc287accbffa8829
Weber Yang <weberyanglalala@gmail.com>
2024-09-17 19:14:34 +0800

    feat(product detail frontend): fetch product detail from web api

--- Dotnet8DifyAgentSample/wwwroot/js/home/index.js
+++ Dotnet8DifyAgentSample/wwwroot/js/home/index.js
@@ -1,16 +1,42 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            productTitle: '',
-            productDescription: '',
-            productCategory: '',
-            productImageId: '',
-            imagePreview: null,
-            imagePreviewStatus: false,
-            productImages: []
-        };
-    },
-    methods: {}
-});
+        data() {
+            return {
+                productTitle: '',
+                productDescription: '',
+                productImageId: '',
+                imagePreview: null,
+                imagePreviewStatus: false,
+                productImagesUrl: '',
+                difyCreateButtonStatus: false,
+            };
+        },
+        methods: {
+            generateProductDetailsByDify() {
+                fetch("api/Dify/CreateWorkflow", {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        "product_name": this.productTitle
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.isSuccess) {
+                            this.productDescription = data.body.description;
+                            this.productImageId = data.body.prompt_id;
+                            this.imagePreviewStatus = true
+                        }
+                    });
+            }
+        },
+        watch: {
+            productTitle: function (val) {
+                this.difyCreateButtonStatus = val.length >= 5 && val.length <= 30;
+            }
+        }
+    })
+;
 
 app.mount('#app');
